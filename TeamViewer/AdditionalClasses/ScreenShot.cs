46584a0d30d5ec0f77a4d25c54de324cfe470d53
camalzade_elvin <camalzade_elvin@outlook.com>
2019-07-10 04:51:37 +0500

    Add project files.

---
+++ TeamViewer/AdditionalClasses/ScreenShot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeamViewer.AdditionalClasses
+{
+    public class ScreenShot
+    {
+        public string GetScreenShotPath(int count)
+        {
+            Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, Screen.PrimaryScreen.Bounds.Size);
+                bmp.Save(@"C:\Users\Documents\source\repos\TeamViewer\TeamViewer\bin\Debug\Images\" + "screenshot" + count.ToString() + ".png");  // saves the image
+            }
+            var source = @"C:\Users\Documents\source\repos\TeamViewer\TeamViewer\bin\Debug\Images\" + "screenshot" + count.ToString() + ".png";
+            return source;
+        }
+    }
+}
---
+++ TeamViewer/Commands/ConnectCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TeamViewer.ViewModels;
+
+namespace TeamViewer.Commands
+{
+    public class ConnectCommand : ICommand
+    {
+        public ConnectCommand(MainViewModel mainViewModel)
+        {
+            MainViewModel = mainViewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+        public MainViewModel MainViewModel { get; set; }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            
+        }
+    }
+}
